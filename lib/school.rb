31d44d677c3199d62865ba1408cc2fef325028fd
Michael Waldo <mbwcogic@yahoo.com>
2019-11-02 22:50:34 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -15,7 +15,7 @@ attr_accessor :roster
       roster[grade] << student
     end
     def grade(student_grade)
-        roster(student_grade)
+        roster[student_grade]
         end      
   end
 end
