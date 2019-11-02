a330630b042e3fafbcea840edf3e424aebc0d5d5
Michael Waldo <mbwcogic@yahoo.com>
2019-11-02 22:38:24 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,19 @@
-# code here!
+require 'pry'
+class School
+attr_accessor :roster 
+  def initialize(school_name)
+      
+      @roster = {}
+  end
+  
+  def add_student(student, grade)
+    #binding.pry
+    if roster[grade]
+       roster[grade] << student
+    else 
+      roster[grade]=[]
+      roster[grade] << student
+    end  
+  end
+end
+
